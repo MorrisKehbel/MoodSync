2fdadc4b87ba35932205c20b4ec17588730178e1
shraddhabhikadiya2017 <shraddhabhikadiya@2017.com>
2025-08-06 16:03:52 +0200

    Added progress of well being bar chart

--- client/src/pages/auth/Dashboard.jsx
+++ client/src/pages/auth/Dashboard.jsx
@@ -1,4 +1,5 @@
 import { PageSlideContainer } from "../../components/shared/wrapper/PageSlideContainer";
+import { EmotionMonthlyAverageChart } from "./dashboard/ProgressBarChart.jsx";
 
 export const Dashboard = () => {
   return (
@@ -14,7 +15,9 @@ export const Dashboard = () => {
 
           {/* Second Row - 1 long box + 1 box same size */}
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            <div className="md:col-span-2 bg-white rounded-lg p-6 shadow-md h-64"></div>
+            <div className="md:col-span-2 bg-white rounded-lg p-6 shadow-md h-64">
+              <EmotionMonthlyAverageChart />
+            </div>
             <div className="bg-white rounded-lg p-6 shadow-md h-64"></div>
           </div>
         </div>
